4cf685d2de3cd04de7a2f658c93e8f745d883095
DESKTOP-I26BE1C\User <marialponce8a@gmail.com>
2024-08-11 15:50:14 -0500

    login de app

--- front-end/src/app/modules/seguridad/login/login.component.ts
+++ front-end/src/app/modules/seguridad/login/login.component.ts
@@ -1,14 +1,39 @@
 import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { GlobalButtonsComponent } from '../../shared/global-buttons/global-buttons.component';
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    MatIconModule
+    ReactiveFormsModule,
+    FormsModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    GlobalButtonsComponent
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  public hidePassword: boolean = false;
+
+  changePasswordVisibility() {
+    this.hidePassword = !this.hidePassword;
+  }
+
+  public handleAction(event: string) {
+    if (event == 'confirm') {
+
+    } else {
+
+    }
+  }
 
 }

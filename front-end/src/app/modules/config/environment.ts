927c1d8dbb3ea8cdd6941cd45a532085bfac8f24
Bomberiux <137364257+Bomberiux@users.noreply.github.com>
2024-09-17 22:12:50 -0500

    correccion de field de busqueda y solventado de merge

--- front-end/src/app/modules/config/environment.ts
+++ front-end/src/app/modules/config/environment.ts
@@ -3,11 +3,7 @@ export class EnvironmentData {
     public static globalUrl = {
         host: 'http://localhost',
         port: '80',
-<<<<<<< HEAD
-        name: '/proyecto-integrador-burbujas-seda'
-=======
         name: '/Lavanderia_Burbuja_Seda'
->>>>>>> feature/Tarea
     }
 
 }
